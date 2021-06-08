1a3b3d58b517adba20fa0c2b71accf1fd5cefdeb
ship <1123804727@qq.com>
2021-06-08 22:58:42 +0800

    二叉树的最大深度

--- src/cn/sp/tree/MaxDepth.java
+++ src/cn/sp/tree/MaxDepth.java
@@ -6,7 +6,7 @@ import java.util.Queue;
 
 /**
  * @Author: Ship
- * @Description: 二叉树的最大深度
+ * @Description: 二叉树的最大深度 https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
  * @Date: Created in 2021/6/8
  */
 public class MaxDepth {
